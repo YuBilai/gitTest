9643f872d9a149ae16dec29986c15d81f80a9526
YuBilai <18129863341@163.com>
2017-05-15 23:18:23 +0800

    文件修改

--- gitTest/ViewController.m
+++ gitTest/ViewController.m
@@ -16,10 +16,14 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
 }
 
 
+
+/**
+ <#Description#>
+ */
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
